8f6e1b50bfefdff5798923fea823b208c9a2270c
Ademola Onasoga <ademola.onasoga@gmail.com>
2022-06-30 23:06:33 +0100

    add timer

---
+++ utili/time.ts
@@ -0,0 +1,9 @@
+const getCurrentTime = (date = new Date()) => {
+
+  const [year, month, day] = [date.getFullYear(), date.getMonth(), date.getDay()];
+  const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+  return {
+    year, month, day, hour, minutes, seconds,
+  };
+};
+export default getCurrentTime;
