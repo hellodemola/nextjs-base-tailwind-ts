54da26f512d47696618e3a4d755b48605d2c3675
Ademola Onasoga <ademola.onasoga@gmail.com>
2023-05-08 05:19:30 +0100

    update test

--- __test__/time.test.ts
+++ __test__/time.test.ts
@@ -1,29 +1,31 @@
-import getCurrentTime from "../utili/time";
+import getCurrentTime from "../utils/time";
 import { renderHook } from "@testing-library/react-hooks";
 import useCurrentTime from "../customHooks/useCurrentTime";
 
 describe("should render time", () => {
-  const time = new Date("10/12/2023");
-  it("Time returns the current time", () => {
-    const isYear = getCurrentTime(time);
-    expect(isYear.year).toBe(2023);
-    expect(isYear.month).toBe(10);
-    expect(isYear.day).toBe(12);
-  });
+  const currentDate = new Date();
 
   it("UseCurrentTime should render current time", () => {
-    const currentDate = new Date();
-    const [current_year, current_month, current_day, current_hour] = [currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), currentDate.getHours()];
+    const [
+      current_year,
+      current_month,
+      current_day,
+      current_hour,
+      current_minutes,
+      ] = [
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate(),
+      currentDate.getHours(),
+      currentDate.getMinutes(),
+    ];
 
-    const { result } = renderHook(() => useCurrentTime(time));
+    const { result } = renderHook(() => useCurrentTime(currentDate));
     expect(result.current.year).toBe(current_year);
     expect(result.current.month).toBe(current_month);
     expect(result.current.day).toBe(current_day);
     expect(result.current.hour).toBe(current_hour);
+    expect(result.current.minutes).toBe(current_minutes);
   });
 
-  it("test useCurrentTime custom hooks", () => {
-    const { result } = renderHook(() => useCurrentTime(time));
-    expect(result.current.year).toBe(2022);
-  });
-});
+})
